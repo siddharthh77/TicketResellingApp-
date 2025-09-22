d7b030d5368c9b208030169a918e5466db967484
siddharthh77 <sid7coc@gmail.com>
2025-09-23 00:41:56 +0530

    Update DBConnection.java

--- TicketApp/src/utils/DBConnection.java
+++ TicketApp/src/utils/DBConnection.java
@@ -1,3 +1,26 @@
 // JDBC connection helper placeholder
 package utils;
-public class DBConnection {}
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    // Change username/password if your SQL Server is different
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=ticket_resell;encrypt=false";
+    private static final String USER = "sa";           // SQL Server username
+    private static final String PASSWORD = "YourPasswordHere"; // Your SQL Server password
+
+    static {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
