8ae6fbf30257716b0a20646ea85e81a3a4fbc206
Sreya Sudevan <sreyasudevan.now@gmail.com>
2025-09-23 10:56:57 +0530

    DeleteTicketServlet.java

--- TicketApp/src/servlets/DeleteTicketServlet.java
+++ TicketApp/src/servlets/DeleteTicketServlet.java
@@ -1,1 +1,32 @@
+package servlets;
+
+import utils.DBConnection;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DeleteTicketServlet")
+public class DeleteTicketServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String ticketId = request.getParameter("id");
+
+        try (Connection con = DBConnection.getConnection()) {
+            String sql = "DELETE FROM Tickets WHERE id=?";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, Integer.parseInt(ticketId));
+            ps.executeUpdate();
+            response.sendRedirect("jsp/myTickets.jsp");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("Error deleting ticket");
+        }
+    }
+}
 
