cf7bc049e1980c1d84072ba58d0800aee5ceb5ad
amith-sreekumar <amithsree2@gmail.com>
2025-10-08 21:48:13 +0530

    Update RegisterServlet.java

--- TicketApp/src/servlets/RegisterServlet.java
+++ TicketApp/src/servlets/RegisterServlet.java
@@ -5,7 +5,6 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.sql.*;
-import org.mindrot.jbcrypt.BCrypt; // Add jBCrypt library to your project
 
 public class RegisterServlet extends HttpServlet {
 
@@ -40,15 +39,12 @@ public class RegisterServlet extends HttpServlet {
                 }
             }
 
-            // Hash the password before storing
-            String hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt());
-
-            // Insert new user
+            // Insert new user (plain-text password)
             String insertSQL = "INSERT INTO [Users] (name, email, password) VALUES (?, ?, ?)";
             try (PreparedStatement ps = con.prepareStatement(insertSQL)) {
                 ps.setString(1, name);
                 ps.setString(2, email);
-                ps.setString(3, hashedPassword);
+                ps.setString(3, password); // plain-text
                 ps.executeUpdate();
             }
 
